1e5efaf7b5b85fcdb8862eb2d461e217d5888ffe
gulla.davide <gulla.davide@BIBLIO-PC08.OSDBSESTO.local>
2020-02-14 12:37:16 +0100

    add R class implements Runnable

--- Thread1/src/thread1/R.java
+++ Thread1/src/thread1/R.java
@@ -3,19 +3,18 @@ package thread1;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class T extends Thread {
-
+public class R implements Runnable {
+    
     private char c;
-
-    public T(char c) {
+    
+    public R(char c) {
         this.c = c;
     }
-
     @Override
     public void run() {
         try {
             while (true) {
-                System.out.println("ciao sono Barney " + c);
+                System.out.println("ciao sono Barney, implemento Runnable " + c);
                 Thread.sleep(2000);
             }
         } catch (InterruptedException ex) {
--- Thread1/src/thread1/Thread1.java
+++ Thread1/src/thread1/Thread1.java
@@ -3,9 +3,15 @@ package thread1;
 public class Thread1 {
 
     public static void main(String[] args) {
-        T t = new T('c');
+        T t = new T('a');
         t.start();
-        T t2 = new T('d');
+        T t2 = new T('b');
         t2.start();
+    R r=new R('c');
+    Thread t3=new Thread(r);
+    t3.start();
+    R r2=new R('d');
+    Thread t4=new Thread(r2);
+    t4.start();
     }
 }
