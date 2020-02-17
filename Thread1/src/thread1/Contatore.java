9a5225fd52573fc4784480c652cb5b93081a4b18
gulla.davide <gulla.davide@BIBLIO-PC01.OSDBSESTO.local>
2020-02-17 12:51:45 +0100

    add synchronized method

--- Thread1/src/thread1/Contatore.java
+++ Thread1/src/thread1/Contatore.java
@@ -5,6 +5,7 @@ public class Contatore {
     public synchronized void stampaContatore() {
         for (int i = 5; i > 0; i--) {
             System.out.println("\tcontatore: " + i);
+       
         }
     }
 }
